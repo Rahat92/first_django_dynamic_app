93656d608ae248c80987ee675d06a00ceab63f89
khrahat92 <khrahat92@gmail.com>
2022-09-11 00:01:53 +0600

    django relational database  creating

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,11 +1,18 @@
+from pyexpat import model
 from statistics import mode
+from turtle import ondrag
+from unicodedata import category
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import  slugify
 # Create your models here.
 
+class Category(models.Model):
+  category = models.CharField(null = True,max_length=50)
+
 class Fruit(models.Model):
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, null = True)
   name = models.CharField(max_length=20)
   description = models.CharField(max_length=150)
   price_per_kg = models.IntegerField(null = True,validators=[MinValueValidator(10),MaxValueValidator(20)])
@@ -14,15 +21,10 @@ class Fruit(models.Model):
   def get_absolute_url(self):
     return reverse("post", args=[self.slug])
     # return reverse("post", kwargs={"pk":self.pk})
-  def save(self,*args, **kwargs):
-    self.slug = slugify(self.name)
-    super().save(*args,**kwargs)
+  # def save(self,*args, **kwargs):
+  #   self.slug = slugify(self.name)
+  #   super().save(*args,**kwargs)
     
   def __str__(self):
     return f'{self.name} : {self.description} , price per kg: {self.price_per_kg}'
 
-# class Category(models.Model):
-#   fruit = models.ForeignKey(Fruit, null = True, blank=True, on_delete=models.CASCADE)
-#   name = models.CharField(max_length=15)
-#   def __str__(self):
-#     return f'{self.name}'
